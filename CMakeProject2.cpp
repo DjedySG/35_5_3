fcd42ef9001067d3969627759c76691cb7a4707a
DjedySG <89001015+DjedySG@users.noreply.github.com>
2022-04-26 22:52:30 +0400

    Add files via upload

---
+++ CMakeProject2.cpp
@@ -0,0 +1,43 @@
+﻿// CMakeProject2.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include "CMakeProject2.h"
+
+struct Record
+{
+	std::string name;
+	std::string family;
+	int age;
+	bool married;
+};
+
+int main()
+{
+	Record record;
+	std::cin >> record.name >> record.family >> record.age >> record.married;
+	std::ofstream file("record.json");
+	nlohmann::json dict;
+	dict["name"] = record.name;
+	dict["family"] = record.family;
+	dict["age"] = record.age;
+	dict["married"] = record.married;
+	file << dict;
+	nlohmann::json dict_second = {
+		{"name", record.name},
+		{"family", record.family},
+		{"age", record.age},
+		{"married", record.married}
+	};
+	file << std::endl << dict_second;
+	file.close();
+
+	std::ifstream file_two("record.json");
+	nlohmann::json dict_third;
+	file_two >> dict_third;
+	std::string name = dict_third["name"];
+	std::cout << name;
+	return 0;
+}
